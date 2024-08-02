4568da080fc412a7c00efc12cc7de3f85eb046af
Mert Topçu <merttopcu95@gmail.com>
2024-08-02 22:22:36 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -10,12 +10,15 @@ namespace PatikaPolymorphismTask
     {
         static void Main(string[] args)
         {
+            //▼ Instance of square and using override method of BaseClass ( GeometricShape )
             Square square = new Square(10,15);
             square.CalculateArea();
 
+            //▼ Instance of square and using override method of BaseClass ( GeometricShape )
             Rectangle rectangle = new Rectangle(20,25);
             rectangle.CalculateArea();
 
+            //▼ Instance of square and using override method of BaseClass ( GeometricShape )
             RightTriangle triangle = new RightTriangle(30,35);
             triangle.CalculateArea();
         }
